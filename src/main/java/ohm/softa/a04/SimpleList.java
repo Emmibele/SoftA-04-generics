597d8fbd8029e4843c07f1e362ffd79f38025da7
Emmibele <120519265+Emmibele@users.noreply.github.com>
2023-04-01 23:03:07 +0200

    Completed First Part >Generic Lists<

--- src/main/java/ohm/softa/a04/SimpleList.java
+++ src/main/java/ohm/softa/a04/SimpleList.java
@@ -1,10 +1,15 @@
 package ohm.softa.a04;
 
-public interface SimpleList extends Iterable {
+public interface SimpleList<T> extends Iterable<T> {
 	/**
 	 * Add a given object to the back of the list.
 	 */
-	void add(Object o);
+	void add(T o);
+
+	/**
+	 * Add default object of given class to list.
+	 */
+	void addDefault(Class<T> c) throws IllegalAccessException, InstantiationException;
 
 	/**
 	 * @return current size of the list
@@ -15,5 +20,5 @@ public interface SimpleList extends Iterable {
 	 * Generate a new list using the given filter instance.
 	 * @return a new, filtered list
 	 */
-	SimpleList filter(SimpleFilter filter);
+	SimpleList<T> filter(SimpleFilter<T> filter);
 }
